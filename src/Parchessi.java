d6e025188857aa45b1e72a41b5093ab4c0f58b72
Dejones1109 <tranvande19980911@gmail.com>
2020-11-24 22:33:28 +0700

    doing frame

--- src/Parchessi.java
+++ src/Parchessi.java
@@ -1,16 +1,32 @@
 import java.awt.AWTException;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 
 @SuppressWarnings("serial")
 class Parchessi extends JFrame {
 
 	public Parchessi() {
+		chooseNumPlayers();
+	}
+	public static void chooseNumPlayers() {
+		String[] options = { "1", "2", "3", "4" };
+		String x = (String) JOptionPane.showInputDialog(null,
+				"How Many Players? (1-4)", "Parcheesi",
+				JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
+//		while (numPlayers > 4 || numPlayers < 1) {
+//			
+//
+//			if (x == null) {
+//				System.exit(0);
+//			} else {
+//				numPlayers = Integer.parseInt(x);
+//			}
+		//}
 
 	}
-
 	public static void main(String[] args) throws AWTException {
-	
+	new Parchessi();
 	}
 
 }
