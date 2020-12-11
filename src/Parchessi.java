97bb1c6cbc5b8f4d3c90d989d57bba2f35ac7379
Cường Trần <46061394+cuonganh@users.noreply.github.com>
2020-12-11 23:46:23 +0700

    update Parchessi
    Thêm các phương thức cần thiết

--- src/Parchessi.java
+++ src/Parchessi.java
@@ -70,7 +70,53 @@ class Parchessi extends JFrame {
 		setBounds(20, 20, 900, 850);
 		setVisible(true);
 	}
+	
+	
+	public static void gameHasBeenWon(int id) {
+		
+	}
+	
+	
+	//Next Turn Returns the index of the next Game Player
+	public static void getNextTurn() {
+		
+	}
+	
+	
+	//Roll Dice. If both Dice are equal, Player can go again
+	public static int roll() {
+		
+	}
+	
+	
+	//Roll Dice Again, can only happen once
+	public static int rollAgain() {
+		
+	}
+	
+	
+	//Choose Number of Players with Error Bounds Checking
+	public static void chooseNumPlayers() {
+		
+	}
+	
+	
+	//Resets the Game
+	public static void reset() {
+		
+	}
+	
+	
+	public static void PlayAI() throws AWTException {
+		
+	}
+	
+	
+	public static Board getBoard() {
+		
+	}
 
+	
 	public static void main(String[] args) throws AWTException {
 		gameFrame = new Parchessi();
 	}
