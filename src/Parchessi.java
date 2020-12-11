da5d5e06eb18e5b7c4948224685bce755434876a
Cuong ACT <bachkhoak60@gmail.com>
2020-12-12 00:47:58 +0700

    update

--- src/Parchessi.java
+++ src/Parchessi.java
@@ -9,10 +9,67 @@ class Parchessi extends JFrame {
 
 	}
 
-	
+
+	public static void gameHasBeenWon(int id) {
+
+	}
+
+
+	/*
+	 * Next Turn Returns the index of the next Game Player
+	 */
+	public static void getNextTurn() {
+
+	}
+
+
+	/*
+	 * Roll Dice. If both Dice are equal, Player can go again
+	 */
+	public static int roll() {
+
+	}
+
+
+	/*
+	 * Roll Dice Again, can only happen once
+	 */
+	public static int rollAgain() {
+
+	}
+
+
+	/*
+	 * Choose Number of Players with Error Bounds Checking
+	 */
 	public static void chooseNumPlayers() {
 
 
+	}
+
+
+	/*
+	 * Resets the Game
+	 */
+	public static void reset() {
+
+	}
+
+
+	public static void PlayAI() throws AWTException {
+
+	}
+
+	public static Board getBoard() {
+
+	}
+
+	/*
+	 * Main Game Method
+	 */
+	public static void main(String[] args) throws AWTException {
+		gameFrame = new Parchessi();
+	
 	}
 
 }
--- src/Player.java
+++ src/Player.java
@@ -12,3 +12,141 @@ import javax.swing.JPanel;
 @SuppressWarnings("serial")
 class Player extends JPanel {
 
+    private final int NUM_TOKENS = 4;
+    private int pid;// Player id
+    private Point p;// Player info,Starting point for pieces for each player
+    private Color color;
+    private int offset;
+    public Token[] t;
+    public final ArrayList<Point> specialPointData;// Point Data for Special
+
+    public Player(int id) {
+
+        specialPointData = new ArrayList<Point>();
+
+        // Initialize Player Details
+        pid = id + 1;
+        switch (id) {
+            case 0:// player1
+
+                break;
+            case 1:// player2
+
+                break;
+            case 2:// player3
+
+                break;
+            case 3:// player4
+
+                break;
+            default: // This should never happen
+                p = new Point(60, 60);
+                color = Color.RED;
+        }
+
+    }
+
+    public ArrayList<String> getAvailableTokens() {
+
+    }
+
+    public int getPID() {
+        return pid;
+    }
+
+    public int getOffset() {
+        return offset;
+    }
+
+    /*
+     * Choose Player's Token to move with Error Bounds Checking
+     */
+    public int chooseToken() {
+
+    }
+
+    public int autoSelectFreeToken() {
+
+    }
+
+    public void setPoint(int x, int y) {
+        p = new Point(x, y);
+    }
+
+    /*
+     * Updates the Player info and tokens on board
+     */
+    public void update(Graphics g) {
+
+    }
+
+    /*
+     * Checks if Player has won. Returns a boolean representation of the condition
+     */
+    public boolean hasWon() {
+
+    }
+
+    /*
+     * gets Player Color
+     */
+    public Color getColor() {
+        return color;
+    }
+
+    /*
+     * Nested Token Object Class, each player has 4 tokens
+     */
+    class Token {
+
+        // This constructor takes the players individual Corner Parameters so it
+        // knows where each players nest is
+        public Token(int i, int x, int y, Color col) {
+
+        }
+
+        // Draws a token, anywhere on the board we want to
+        public void drawToken(Graphics g, int x, int y) {
+
+        }
+
+        public void draw(Graphics g) {
+
+        }
+
+        /*
+         * If the player has traversed the entire board, then we want to go down the
+         * players safe zone for a win! Returns -1 if false Returns any other positive
+         * value to represent how many spaces the user can traverse following the turn
+         */
+        public int checkTraversal() {
+
+        }
+
+        /*
+         * gets the safe zone condition
+         */
+        public boolean getSafeZone() {
+            return lastEight;
+        }
+
+        /*
+         * sets the safe zone condition
+         */
+        public void setSafeZone(boolean b) {
+            lastEight = b;
+        }
+
+        /*
+         * Checks to see if the token is Safe, or not
+         */
+        public boolean isSafe() {
+
+        }
+
+
+    // Getter and Setter
+
+    }
+
+}
