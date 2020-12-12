85fdb9ff83be5b0a9e341de082fa260d9e720d4c
vuxuan2511 <vuxuan183@gmail.com>
2020-12-12 09:02:29 +0700

    menu done

--- src/Menu.java
+++ src/Menu.java
@@ -1,4 +1,4 @@
-import java.awt.AWTException;
+package src;
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.event.ActionEvent;
@@ -16,23 +16,48 @@ class Menu extends JPanel {
 	public Menu() {
 		setBackground(new Color(72, 209, 204));
 
+		/*
+		 * Add Basic Menu Buttons, Other Buttons Need access to internal logic
+		 * of the game, so they are added in the parcheesi constructor
+		 */
+
+		addButton(this, "Help/Instructions", null, new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				String info = "Goal: \n\tGet all of your tokens arround the board,"
+						+ " and then to the center of the board.\n\nRules:\n\tIf a token is on a white tile,"
+						+ " and another Player's token is moved there, the origonal token must"
+						+ " go back to start. \nEach player will roll 2 dice, with a maximum value of 12 to determine"
+						+ " how many spaces to move\n\n"
+						+ "For Each Player's Turn: \n\t1.First Roll the dice.\n\t2.Then Select Which piece to move.\n "
+						+ "WARNING! If you select a peice before the roll, this piece will be moved to that location.";
+				JOptionPane.showMessageDialog(null, info, "Help/Instuctions",
+						JOptionPane.INFORMATION_MESSAGE);
+			}
+		});
+
+		addButton(this, "Exit Game", null, new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				if (areYouSure()) {
+					System.exit(0);
+				}
+			}
+		});
+
+		addButton(this, "New Game", null, new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				if (areYouSure()) {
+					Parchessi.reset();
+				}
+			}
+		});
+
 	}
-
-	// Generates a simple button
-	public void addButton() {
-
-	}
-
-	// Yes/No Dialog Generator for basic yes/no menu questions
-	public boolean areYouSure() {
-
-	}
-
 
 	/*
 	 * Generates a simple button
 	 */
-	public void addButton(Container c, String title, ImageIcon img, ActionListener a) {
+	public void addButton(Container c, String title, ImageIcon img,
+			ActionListener a) {
 		JButton b = new JButton(title);
 		c.add(b);
 		b.addActionListener(a);
@@ -42,8 +67,15 @@ class Menu extends JPanel {
 	 * Yes/No Dialog Generator for basic yes/no menu questions
 	 */
 	public boolean areYouSure() {
+		String[] options = { "Yes!", "No!" };
+		String x = (String) JOptionPane.showInputDialog(null, "Are You Sure?",
+				"Parcheesi", JOptionPane.QUESTION_MESSAGE, null, options,
+				options[0]);
+		if (x == "Yes!") {
+			return true;
+		} else {
+			return false;
+		}
 
 	}
-
-
 }
