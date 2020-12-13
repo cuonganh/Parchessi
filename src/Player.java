04c9f13f4ff9835a9369b07fb14da1feb4c66374
Cường Trần <46061394+cuonganh@users.noreply.github.com>
2020-12-13 18:03:21 +0700

    update
    parameter and format

--- src/Player.java
+++ src/Player.java
@@ -12,154 +12,159 @@ import javax.swing.JPanel;
 @SuppressWarnings("serial")
 class Player extends JPanel {
 
-    private final int NUM_TOKENS = 4;
-    private int pid;// Player id
-    private Point p;// Player info,Starting point for pieces for each player
-    private Color color;
-    private int offset;
-    public Token[] t;
-    public final ArrayList<Point> specialPointData;// Point Data for Special
+	private final int NUM_TOKENS = 4;
+    	private int pid;// Player id
+    	private Point p;// Player info,Starting point for pieces for each player
+    	private Color color;
+	private Color red = new Color(255, 0, 0);
+	private Color blue = new Color(0, 0, 255);
+	private Color green = new Color(0, 255, 0);
+	private Color yellow = new Color(255, 255, 0);
+	private Color[] col = {red,blue,green,yellow};
+	private int offset;
+	public Token[] t;
+	public final ArrayList<Point> specialPointData;// Point Data for Special
 
-    public Player(int id) {
+    	public Player(int id) {
 
-        specialPointData = new ArrayList<Point>();
+        	specialPointData = new ArrayList<Point>();
 
-        // Initialize Player Details
-        pid = id + 1;
-        switch (id) {
-            case 0:// player1
-                p = new Point(60, 30);
-		offset = 25;// Perfect
-		color = Color.RED;
-                break;
-            case 1:// player2
-                p = new Point(514, 30);
-                offset = 31;
-                color = Color.BLUE;
-                break;
-            case 2:// player3
-                p = new Point(60, 485);
-                offset = -1;
-                color = Color.YELLOW;               
-                break;
-            case 3:// player4
-                p = new Point(514, 485);
-                offset = 47;
-                color = Color.GREEN;
-                break;
-            default: // This should never happen
-                p = new Point(60, 60);
-                color = Color.RED;
-        }
+        	// Initialize Player Details
+        	pid = id + 1;
+        	switch (id) {
+            	case 0:// player1
+			p = new Point(60, 30);
+			offset = 25;// Perfect
+			color = Color.RED;
+			break;
+		    case 1:// player2
+			p = new Point(514, 30);
+			offset = 31;
+			color = Color.BLUE;
+			break;
+		    case 2:// player3
+			p = new Point(60, 485);
+			offset = -1;
+			color = Color.YELLOW;               
+			break;
+		    case 3:// player4
+			p = new Point(514, 485);
+			offset = 47;
+			color = Color.GREEN;
+			break;
+		    default: // This should never happen
+			p = new Point(60, 60);
+			color = Color.RED;
+		}
 	    
-	t = new Token[NUM_TOKENS];
-	for (int i = 0; i < NUM_TOKENS; i++) {
-		t[i] = new Token(i, p.x, p.y, getColor());
+		t = new Token[NUM_TOKENS];
+		for (int i = 0; i < NUM_TOKENS; i++) {
+			t[i] = new Token(i, p.x, p.y, getColor());
+		}
+	    
+    	}
+
+	public ArrayList<String> getAvailableTokens() {
+
 	}
-	    
-    }
 
-    public ArrayList<String> getAvailableTokens() {
+	public int getPID() {
+	return pid;
+	}
 
-    }
+	public int getOffset() {
+	return offset;
+	}
 
-    public int getPID() {
-        return pid;
-    }
+	/*
+	* Choose Player's Token to move with Error Bounds Checking
+	*/
+	public int chooseToken() {
 
-    public int getOffset() {
-        return offset;
-    }
+	}
 
-    /*
-     * Choose Player's Token to move with Error Bounds Checking
-     */
-    public int chooseToken() {
+	public int autoSelectFreeToken() {
 
-    }
+	}
 
-    public int autoSelectFreeToken() {
+	public void setPoint(int x, int y) {
+	p = new Point(x, y);
+	}
 
-    }
+	/*
+	* Updates the Player info and tokens on board
+	*/
+	public void update(Graphics g) {
 
-    public void setPoint(int x, int y) {
-        p = new Point(x, y);
-    }
+	}
 
-    /*
-     * Updates the Player info and tokens on board
-     */
-    public void update(Graphics g) {
+	/*
+	* Checks if Player has won. Returns a boolean representation of the condition
+	*/
+	public boolean hasWon() {
 
-    }
+	}
 
-    /*
-     * Checks if Player has won. Returns a boolean representation of the condition
-     */
-    public boolean hasWon() {
+	/*
+	* gets Player Color
+	*/
+	public Color getColor() {
+	return color;
+	}
 
-    }
+	/*
+	* Nested Token Object Class, each player has 4 tokens
+	*/
+	class Token {
 
-    /*
-     * gets Player Color
-     */
-    public Color getColor() {
-        return color;
-    }
+	// This constructor takes the players individual Corner Parameters so it
+	// knows where each players nest is
+	public Token(int i, int x, int y, Color col) {
 
-    /*
-     * Nested Token Object Class, each player has 4 tokens
-     */
-    class Token {
+	}
 
-        // This constructor takes the players individual Corner Parameters so it
-        // knows where each players nest is
-        public Token(int i, int x, int y, Color col) {
+	// Draws a token, anywhere on the board we want to
+	public void drawToken(Graphics g, int x, int y) {
 
-        }
+	}
 
-        // Draws a token, anywhere on the board we want to
-        public void drawToken(Graphics g, int x, int y) {
+	public void draw(Graphics g) {
 
-        }
+	}
 
-        public void draw(Graphics g) {
+	/*
+	 * If the player has traversed the entire board, then we want to go down the
+	 * players safe zone for a win! Returns -1 if false Returns any other positive
+	 * value to represent how many spaces the user can traverse following the turn
+	 */
+	public int checkTraversal() {
 
-        }
+	}
 
-        /*
-         * If the player has traversed the entire board, then we want to go down the
-         * players safe zone for a win! Returns -1 if false Returns any other positive
-         * value to represent how many spaces the user can traverse following the turn
-         */
-        public int checkTraversal() {
+	/*
+	 * gets the safe zone condition
+	 */
+	public boolean getSafeZone() {
+	    return lastEight;
+	}
 
-        }
+	/*
+	 * sets the safe zone condition
+	 */
+	public void setSafeZone(boolean b) {
+	    lastEight = b;
+	}
 
-        /*
-         * gets the safe zone condition
-         */
-        public boolean getSafeZone() {
-            return lastEight;
-        }
+	/*
+	 * Checks to see if the token is Safe, or not
+	 */
+	public boolean isSafe() {
 
-        /*
-         * sets the safe zone condition
-         */
-        public void setSafeZone(boolean b) {
-            lastEight = b;
-        }
-
-        /*
-         * Checks to see if the token is Safe, or not
-         */
-        public boolean isSafe() {
-
-        }
+	}
 
 
-    // Getter and Setter
+	// Getter and Setter
 
-    }
+		
 
 }
