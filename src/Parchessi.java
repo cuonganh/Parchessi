a86b88a0589e3026d86f22eda879ea06ff2fc5d7
Cường Trần <46061394+cuonganh@users.noreply.github.com>
2020-12-15 19:48:19 +0700

    import Swing

--- src/Parchessi.java
+++ src/Parchessi.java
@@ -1,7 +1,12 @@
 import java.awt.Color;
 import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 
 @SuppressWarnings("serial")
 class Parchessi extends JFrame {
