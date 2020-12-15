f913b2c3af84580c0917114e764dba8cb450c5c7
Cường Trần <46061394+cuonganh@users.noreply.github.com>
2020-12-15 19:48:57 +0700

    update inport
    import awt

--- src/Parchessi.java
+++ src/Parchessi.java
@@ -1,5 +1,12 @@
+package src;
+import java.awt.AWTException;
 import java.awt.Color;
+import java.awt.Robot;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
