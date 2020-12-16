2976aba1e79722a78e0e1aa0fc46160f3467f2b0
nguyenhoangnhung <nguyenhoangnhung1998@gmail.com>
2020-12-16 09:42:26 +0700

    drawToken

--- src/Player.java
+++ src/Player.java
@@ -189,11 +189,38 @@ class Player extends JPanel {
 
 		// Draws a token, anywhere on the board we want to
 		public void drawToken(Graphics g, int x, int y) {
-
+			g.setColor(c);
+			g.fillRect(x, y, tokenSize, tokenSize);
+			g.setColor(Color.BLACK);
+			g.drawRect(x, y, tokenSize, tokenSize);
+			g.drawString(Integer.toString(id), x + 7, y + 15);
 		}
 
 		public void draw(Graphics g) {
-
+			// If not on board(index 0), draw within it's corresponding box
+			if (index == 0) {
+				switch (id) {
+				case 0:
+					drawToken(g, p.x + 30, p.y + 30);
+					break;
+				case 1:
+					drawToken(g, p.x + 60, p.y + 30);
+					break;
+				case 2:
+					drawToken(g, p.x + 30, p.y + 60);
+					break;
+				case 3:
+					drawToken(g, p.x + 60, p.y + 60);
+					break;
+				default:
+				}
+			} else {
+				if (getPositionIndex() >= 63) {// Else Draw Safe zone
+					drawToken(g, 335 + 10, 355 + 10);
+				} else {// Else Draw on Board
+					drawToken(g, getX(), getY());
+				}
+			}
 		}
 
 		/*
